e9952c0351d1c158d3d819aa2537a377af58a688
khaledgomah <khaledgomah10@gmail.com>
2024-12-10 17:04:05 +0200

    refactor code

--- lib/features/time/presentation/views/home_view.dart
+++ lib/features/time/presentation/views/home_view.dart
@@ -1,8 +1,6 @@
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
-import 'package:silent_space/features/time/presentation/views/widgets/custom_count_down_timer.dart';
-import 'package:silent_space/features/time/presentation/views/widgets/restart_icon_button.dart';
-import 'package:silent_space/features/time/presentation/views/widgets/start_and_puase_widget.dart';
+import 'package:silent_space/features/time/presentation/views/widgets/timer_section.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -22,29 +20,7 @@ class _HomeViewState extends State<HomeView> {
         backgroundColor: Colors.green,
         title: const Text('Home'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomCountDownTimer(countDownController: _countDownController, maxTime: maxTime),
-          const SizedBox(height: 50),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              StartAndPuaseWidget(
-                countDownController: _countDownController,
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              RestartIconButton(
-                countDownController: _countDownController,
-                maxTime: maxTime,
-              )
-            ],
-          ),
-        ],
-      ),
+      body: TimerSection(countDownController: _countDownController, maxTime: maxTime),
     );
   }
 }
-
